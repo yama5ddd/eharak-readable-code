6bfe9ba9f902fd25bc7845ccce5344642cbbba11
Kensuke Ehara <kensuke.ehara@justsystems.com>
2019-09-24 11:25:07 +0900

    spec1: 単語情報を出力できるようにする

---
+++ eharak-readable-code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace eharak_readable_code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("上手");
+        }
+    }
+}

908b6fd50920d05d34497fbebc3c888a4af14d9c
Kensuke Ehara <kensuke.ehara@justsystems.com>
2019-09-24 13:09:31 +0900

    spec3: 入力引数のエラー処理を追加

--- eharak-readable-code/Program.cs
+++ eharak-readable-code/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
 
 namespace eharak_readable_code
@@ -7,6 +8,16 @@ namespace eharak_readable_code
     {
         static void Main(string[] args)
         {
+            if (args.Length <= 0)
+            {
+                var appFilePath = Process.GetCurrentProcess().MainModule.FileName;
+                var appFileName = Path.GetFileName(appFilePath);
+                var errorMessage = string.Format("使い方 : {0} データファイル名のフルパス", appFileName);
+
+                Console.WriteLine(errorMessage);
+                return;
+            }
+
             foreach (var line in File.ReadLines(args[0]))
             {
                 Console.WriteLine(line);

223ec2c69ba56b40210b08936444a2bf3786a78c
yamakawa <16297129+yama5ddd@users.noreply.github.com>
2019-09-24 15:20:18 +0900

    spec6: 指定されたIDの単語のみを出力できるようにする

--- eharak-readable-code/Program.cs
+++ eharak-readable-code/Program.cs
@@ -12,7 +12,7 @@ namespace eharak_readable_code
         /// <param name="args">コマンドライン引数</param>
         static void Main(string[] args)
         {
-            if (args.Length <= 0)
+            if (args.Length <= 0 || args.Length > 2)
             {
                 PrintUsage();
                 return;
@@ -20,11 +20,22 @@ namespace eharak_readable_code
 
             try
             {
+                var hasTargetId = (args.Length == 2);
+                int targetId = hasTargetId ? int.Parse(args[1]) : 0;
+
                 // 入力データファイルの登録順(行数)をIDとする
                 int id = 0;
                 foreach (var line in File.ReadLines(args[0]))
                 {
-                    Console.WriteLine($"{++id}: {line}");
+                    ++id;
+
+                    if (hasTargetId)
+                    {
+                        if (id != targetId)
+                            continue;
+                    }
+
+                    Console.WriteLine($"{id}: {line}");
                 }
             }
             catch (Exception ex)
@@ -42,9 +53,10 @@ namespace eharak_readable_code
         {
             var appFilePath = Process.GetCurrentProcess().MainModule.FileName;
             var appFileName = Path.GetFileName(appFilePath);
-            var errorMessage = string.Format("使い方 : {0} データファイル名のフルパス", appFileName);
 
-            Console.WriteLine(errorMessage);
+            Console.WriteLine("使い方 : ");
+            Console.WriteLine($"すべての単語を出力する : {appFileName} データファイル名のフルパス");
+            Console.WriteLine($"指定したIDの単語を出力する : {appFileName} データファイル名のフルパス 出力する単語のID");
         }
     }
 }
